c87e68d2c79c8e4135348751e9826b17679ccbbd
vincemonte <vincentmonteleone99@gmail.com>
2024-04-05 17:10:14 -0400

    Fixed issue with Lusitana font name.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitania } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 
@@ -16,7 +16,7 @@ export default function Page() {
         <div
              className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
     />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitania.className}`}>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
